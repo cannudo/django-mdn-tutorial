8373268f3f9669a29c3189eef0404aeee3bf0808
Luan C. Redmann <luandacostaredmann@gmail.com>
2021-07-27 00:06:00 -0300

    💥 página index com conteúdo dinâmico

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
 
-# Create your views here.
+    num_instances_available = BookInstance.objects.filter(status__exact = 'A').count()
+
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context = context)
